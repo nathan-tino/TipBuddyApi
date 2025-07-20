b5043650d65c9f41f9d5eb49141dc45aebd19a50
Nathan Tino <tinonw01@gmail.com>
2025-07-19 23:54:51 -0700

    reduce accuracy of shift.date in database

---
+++ TipBuddyApi/Migrations/20250720064737_ChangeShiftDatePrecision.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TipBuddyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeShiftDatePrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Shifts",
+                type: "datetime2(0)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Shifts",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(0)");
+        }
+    }
+}
