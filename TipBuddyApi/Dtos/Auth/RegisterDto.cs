5d8eb77b4c85b2cc134e4a85ac7bb24eef9153d0
Nathan Tino <tinonw01@gmail.com>
2025-09-05 17:24:07 -0700

    Change PhoneNumber type to string in RegisterDto
    Updated the `PhoneNumber` property in the `RegisterDto` class from a nullable integer (`int?`) to a nullable string (`string?`). This modification allows for the inclusion of non-numeric characters, enhancing flexibility in phone number representation.

--- TipBuddyApi/Dtos/Auth/RegisterDto.cs
+++ TipBuddyApi/Dtos/Auth/RegisterDto.cs
@@ -4,7 +4,7 @@ namespace TipBuddyApi.Dtos.Auth
     {
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
-        public int? PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         public required string UserName { get; set; }
         public required string Email { get; set; }
 
